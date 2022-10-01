86fe2a96a90295dde294ad6c7fc0f23f9f1d6a86
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-01 13:55:57 +0700

    testing

--- 003 - Data Type Casting/main.py
+++ 003 - Data Type Casting/main.py
@@ -9,7 +9,7 @@ print("data = ", data_int, ",type =", type(data_int))
 
 data_float = float(data_int)
 data_str = str(data_int)
-data_bool = bool(data_int)  # akan false jika nilai int = 0
+data_bool = bool(data_int)  # akan false jika nilai int = 0, selain 0 akan true
 print("data = ", data_float, ",type =", type(data_float))
 print("data = ", data_str, ",type =", type(data_str))
 print("data = ", data_bool, ",type =", type(data_bool))
